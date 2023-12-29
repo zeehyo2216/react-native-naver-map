e32f9f67af9181074144c2b51e3b5f3f5b35f164
Jung Zee Hyo <91977327+zeehyo2216@users.noreply.github.com>
2023-12-29 15:46:52 +0900

    Update RNNaverMapCircleOverlay.m

--- ios/reactNativeNMap/RNNaverMapCircleOverlay.m
+++ ios/reactNativeNMap/RNNaverMapCircleOverlay.m
@@ -10,7 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTUtils.h>
 #import <NMapsMap/NMFNaverMapView.h>
-#import <NMapsMap/NMGLatLng.h>
+#import <NMapsGeometry/NMGLatLng.h>
 #import <NMapsMap/NMFCircleOverlay.h>
 
 #import "RCTConvert+NMFMapView.h"
