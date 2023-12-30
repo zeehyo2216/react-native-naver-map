c511afaf863cffe2a4dad821533d3aee59fd6e2a
Jung Zee Hyo <91977327+zeehyo2216@users.noreply.github.com>
2023-12-30 17:03:42 +0900

    Update RNNaverMapPolygonOverlay.h

--- ios/reactNativeNMap/RNNaverMapPolygonOverlay.h
+++ ios/reactNativeNMap/RNNaverMapPolygonOverlay.h
@@ -8,7 +8,7 @@
 
 #import <React/RCTBridge.h>
 #import <React/RCTComponent.h>
-#import <NMapsMap/NMGLatLng.h>
+#import <NMapsGeometry/NMGLatLng.h>
 #import <NMapsMap/NMFPolygonOverlay.h>
 
 #import "RCTConvert+NMFMapView.h"
